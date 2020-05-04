97d197a97c4a49669d41edbac9a3ee0bfaf2c824
Volcomix <Volcomix@users.noreply.github.com>
2020-05-04 19:51:40 +0200

    Updates

---
+++ precache-manifest.736ee052faa3045ce9e176d903122fc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a04e968d11049752f6b7921ee320508",
+    "url": "/i-coach/index.html"
+  },
+  {
+    "revision": "b652f12dcd28524c8eb7",
+    "url": "/i-coach/static/js/2.6fbef380.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/i-coach/static/js/2.6fbef380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33bddefc4bdd7c84c903",
+    "url": "/i-coach/static/js/main.53f9c66f.chunk.js"
+  },
+  {
+    "revision": "af627974cb76d6120d0b",
+    "url": "/i-coach/static/js/runtime-main.ece4eef5.js"
+  }
+]);
