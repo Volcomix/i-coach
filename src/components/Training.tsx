a98fbe0ec88dc05fa0608ba240ce443996d54979
Volcomix <Volcomix@users.noreply.github.com>
2020-06-16 15:34:56 +0200

    Update timer periodically

--- src/components/Training.tsx
+++ src/components/Training.tsx
@@ -1,8 +1,13 @@
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import exercises from '../exercises'
+
+enum IntervalType {
+  Prepare,
+  Work,
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -44,27 +49,58 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function Training() {
   const classes = useStyles()
 
-  const [exerciseId] = useState(0)
+  const [exerciseId, setExerciseId] = useState(0)
+  const [intervalType, setIntervalType] = useState(IntervalType.Prepare)
+  const [time, setTime] = useState(0)
 
   const exercise = exercises[exerciseId]
+
+  const maxTime =
+    intervalType === IntervalType.Prepare
+      ? exercise.prepareTime
+      : exercise.workTime
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime((prevTime) => prevTime + 1)
+    }, 1000)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (time === maxTime) {
+      if (intervalType === IntervalType.Prepare) {
+        setIntervalType(IntervalType.Work)
+      } else {
+        setExerciseId(exerciseId + 1)
+        setIntervalType(IntervalType.Prepare)
+      }
+      setTime(0)
+    }
+  }, [exerciseId, intervalType, time, maxTime])
 
   return (
     <div className={classes.root}>
       <Typography className={classes.intervalTime} variant="h1">
-        {exercise.prepareTime}
+        {maxTime - time}
         <div className={classes.intervalProgress}>
           <CircularProgress
             variant="static"
             size="100%"
             thickness={1.5}
-            value={50}
+            value={(100 * time) / maxTime}
           />
         </div>
       </Typography>
       <div className={classes.exercise}>
-        <Typography variant="h4" color="textSecondary">
-          Next
-        </Typography>
+        {intervalType === IntervalType.Prepare && (
+          <Typography variant="h4" color="textSecondary">
+            Next
+          </Typography>
+        )}
         <Typography className={classes.exerciseName} variant="h3">
           {exercise.name}
         </Typography>
